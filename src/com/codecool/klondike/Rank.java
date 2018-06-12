1d26c74917aeba9dc9cbce8ae860e2779d9726a3
Mezii <mezitomi@gmail.com>
2018-06-12 11:28:45 +0200

    Switches of ranks and suits changed to enums

---
+++ src/com/codecool/klondike/Rank.java
@@ -0,0 +1,15 @@
+package com.codecool.klondike;
+
+public enum Rank {
+    TWO("2"), THREE("3"), FOUR("4"), FIVE("5"), SIX("6"), SEVEN("7"), EIGHT("8"), NINE("9"), TEN("10"), JUMBO("11"), QUEEN("12"), KING("13"), ACE("1");
+
+    private final String name;
+
+    Rank(String str) {
+        this.name = str;
+    }
+
+    public String toString() {
+        return name;
+    }
+}
